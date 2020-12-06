a83be5f848b9ebc1ea26a1c8e5e99715232ce9b9
Kole Morgan <kmorgan0057@gmail.com>
2020-12-06 17:18:32 -0600

    Java Exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,28 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.print("Hello ");
-        System.out.print("World!");
+        System.out.println("World!");
+        int favNum = 37;
+        System.out.println(favNum);
+        String myString = "This is my string.";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        x = 4;
+        x+= 5;
+
+        x=3;
+        int y = 4;
+        y*= x;
+        System.out.println(y);
+
+
+
+
     }
 }
