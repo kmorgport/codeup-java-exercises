2aa56f51f5c38fe4c4b57a1a8cc3eb051b0879f5
Kole Morgan <kmorgan0057@gmail.com>
2020-12-12 14:59:19 -0600

    Output

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,26 +1,39 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.print("Hello ");
-        System.out.println("World!");
-        int favNum = 37;
-        System.out.println(favNum);
-        String myString = "This is my string.";
-        System.out.println(myString);
-        double myNumber = 3.14;
-        System.out.println(myNumber);
+//        System.out.println("Hello World!");
+//        System.out.print("Hello ");
+//        System.out.println("World!");
+//        int favNum = 37;
+//        System.out.println(favNum);
+//        String myString = "This is my string.";
+//        System.out.println(myString);
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        x = 4;
+//        x+= 5;
+//
+//        x=3;
+//        int y = 4;
+//        y*= x;
+//        System.out.println(y);
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+        String name = "codeup";
+        System.out.printf("Hello there, %s. Nice to see you.\n",name);
 
-        x = 4;
-        x+= 5;
+        Scanner scanner = new Scanner(System.in);
 
-        x=3;
-        int y = 4;
-        y*= x;
-        System.out.println(y);
+        System.out.print("Enter something: ");
+        String userInput =  scanner.next();
+
+        System.out.println("You entered: -->\""+ userInput+"\"<--");
+
+
 
 
 
