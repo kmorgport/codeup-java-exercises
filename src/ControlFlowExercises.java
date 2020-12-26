f0aaa732e1eb54b0df01d5f0fedb35170b4a6334
Kole Morgan <kmorgan0057@gmail.com>
2020-12-26 13:47:12 -0600

    Java Flow Control Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,25 +1,30 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        int i = 5;
-        while(i<=15){
-            System.out.println("i is "+ i);
-            i++;
-        }
-        for(int j = 100; j>=-10; j-=5){
-            System.out.println(j);
-        }
+//        int i = 5;
+//        while(i<=15){
+//            System.out.println("i is "+ i);
+//            i++;
+//        }
+//        for(int j = 100; j>=-10; j-=5){
+//            System.out.println(j);
+//        }
 
-        for(int k =1; k<=100; k++){
-            if(k%3==0 && k%5==0){
-                System.out.println("FizzBuzz");
-            }else if(k%3==0){
-                System.out.println("Fizz");
-            }else if(k%5==0){
-                System.out.println("Buzz");
-            }else{
-                System.out.println(k);
-            }
-        }
+        long a = 2; do{
+            System.out.println(a);
+            a = a*a;
+        }while(a<1000000);
+
+//        for(int k =1; k<=100; k++){
+//            if(k%3==0 && k%5==0){
+//                System.out.println("FizzBuzz");
+//            }else if(k%3==0){
+//                System.out.println("Fizz");
+//            }else if(k%5==0){
+//                System.out.println("Buzz");
+//            }else{
+//                System.out.println(k);
+//            }
+//        }
 
     }
 }
