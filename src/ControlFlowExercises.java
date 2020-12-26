753f36dd6cdb268098f23ea4d1ed9cee353a4415
Kole Morgan <kmorgan0057@gmail.com>
2020-12-26 12:52:21 -0600

    Java Control Flow Exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+        int i = 5;
+        while(i<=15){
+            System.out.println("i is "+ i);
+            i++;
+        }
+        int j = 0;
+        do{
+            System.out.println(j);
+                    j+=2;
+        }while(j<=100);
+
+    }
+}
