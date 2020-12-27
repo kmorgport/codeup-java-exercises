e37b7d93e1434cfcd8d4b5d28e89f98c89b3d678
Kole Morgan <kmorgan0057@gmail.com>
2020-12-27 17:49:32 -0600

    Java String Practice

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String string = "We don't need no education";
+        System.out.println(string);
+        string = string.replace("education","thought control");
+        System.out.println(string);
+        string = "Check \"this\" out! \"s inside of \"s!";
+        System.out.println(string);
+        string = "In windows, the main drive is usually C:\\";
+        System.out.println(string);
+        string ="I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash.\\\\\\!";
+        System.out.println(string);
+    }
+}
