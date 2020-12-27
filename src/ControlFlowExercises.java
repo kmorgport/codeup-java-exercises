74e48b5e7155e57044c27fe0a10b68cb39504507
Kole Morgan <kmorgan0057@gmail.com>
2020-12-27 12:45:59 -0600

    Java Control Flow Exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -36,6 +36,23 @@ public class ControlFlowExercises {
         for(int z=1; z<=integer; z++){
             System.out.format("%1s%10d%10s%n", z, z*z, z*z*z);
         }
-
-    }
+        boolean gradestatus = true;
+        while(gradestatus) {
+            System.out.print("What is your number grade? ");
+            int grade = scanner.nextInt();
+            if (grade <= 100 && grade >= 88) {
+                System.out.println("Your grade is an A");
+            } else if (grade <= 87 && grade >= 80) {
+                System.out.println("Your grade is a B");
+            } else if (grade <= 79 && grade >= 67) {
+                System.out.println("Your grade is a C");
+            } else if (grade <= 66 && grade >= 60) {
+                System.out.println("Your grade is a D");
+            } else if (grade <= 65 && grade >= 0) {
+                System.out.println("We're sorry, this is a failing grade.");
+            }
+            System.out.print("Would you like to continue? y/n ");
+            String userInput = scanner.next();
+            gradestatus = userInput.equals("y");
+    }}
 }
