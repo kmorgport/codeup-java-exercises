fd2fe1dd82da4370e546433e8653688d6b6487c9
Kole Morgan <kmorgan0057@gmail.com>
2021-01-21 16:43:24 -0600

    java exercises

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -21,12 +21,62 @@ public class JavaPractice {
         }
 
     }
+
+    public static String theEnd(String str, boolean front){
+        String a = str.substring(0,1);
+        String b = str.substring(str.length()-1);
+        if(front){
+            return a;
+        }else{
+            return b;
+        }
+    };
+
+//    public static String withoutEnd(String str) {
+//        String a = str.substring(1,str.length()-1);
+//        if(str.length()<=2){
+//            return "";
+//        }else{
+//            return a;
+//        }
+//    }
+
+    public static String withoutEnd(String str) {
+        if(str.length()>2){
+            String a = str.substring(1,str.length()-1);
+            return a;
+        }else{
+            return "";
+        }
+    }
+
+    public static String middleTwo(String str){
+        String a = str.substring(0,str.length()/2);
+        String b = str.substring(str.length()/2);
+        String c = a.substring(a.length()-1);
+        String d = b.substring(0,1);
+        return c+d;
+    }
     public static void main(String[] args){
-        String hello = right2("Hello");
-        System.out.println(hello);
-        String onetwo = right2("12345");
-        System.out.println(onetwo);
-        String cat = right2("cat");
-        System.out.print(cat);
+//        String hello = right2("Hello");
+//        System.out.println(hello);
+//        String onetwo = right2("12345");
+//        System.out.println(onetwo);
+//        String cat = right2("cat");
+//        System.out.print(cat);
+//        String hel = theEnd("Hello",true);
+//        System.out.println(hel);
+//        String hello = withoutEnd("Hello");
+//        System.out.println(hello);
+//        String abc = withoutEnd("abc");
+//        System.out.println(abc);
+//        String ab = withoutEnd("a");
+//        System.out.println(ab);
+        String string = middleTwo("string");
+        System.out.println(string);
+        String practice = middleTwo("practice");
+        System.out.println(practice);
+        String code = middleTwo("code");
+        System.out.println(code);
     }
 }
