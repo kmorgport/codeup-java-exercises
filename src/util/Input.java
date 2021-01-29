9fc57142d48c1d835b1737052667711acf22236f
Kole Morgan <kmorgan0057@gmail.com>
2021-01-28 20:13:47 -0600

    java classes

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,33 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
-    private String scanner;
+    protected Scanner scanner;
+
+    public Input(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString(){
+        System.out.println("Enter a string:");
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo(){
+        System.out.println("Enter yes or no");
+        String answer = scanner.nextLine();
+        return answer.toLowerCase().equals("yes") || answer.toLowerCase().equals("y");
+    }
+
+    public int getInt(){
+        System.out.println("Enter an integer");
+        return scanner.nextInt();
+    }
+
+    public double getDouble(){
+        System.out.println("Enter a double: ");
+        return scanner.nextDouble();
+    }
+
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,12 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args){
+        Input input = new Input();
+//        System.out.println(input.getString());
+//        System.out.println(input.getInt());
+//        System.out.println(input.getDouble());
+        System.out.println(input.yesNo());
+
+    }
+}
