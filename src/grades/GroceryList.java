4e72d91a2e3853536eed9c4b755de47abc9c1ae1
Kole Morgan <kmorgan0057@gmail.com>
2021-02-07 13:44:54 -0600

    Grocery List exercise

--- src/grades/GroceryList.java
+++ src/grades/GroceryList.java
@@ -1,13 +1,47 @@
 package grades;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import util.Input;
 
 public class GroceryList {
     protected HashMap<String,Integer> groceries;
 
+    public static void main(String[] args){
+        Input input = new Input();
+        GroceryList list = new GroceryList();
+        System.out.println("Enter a food");
+        String answ = input.getString();
+        System.out.println("Enter how much");
+        int quantity = input.getInt();
+        list.addToHash(answ,quantity);
+        for(Map.Entry entry: list.groceries.entrySet()){
+            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
+        }
+
+
+    }
+
     public GroceryList(){
     this.groceries = new HashMap<>();
+    }
+
+    public static void chooseGrocery(){
+        Input input = new Input();
+        boolean ans = input.yesNo();
+        GroceryList list = new GroceryList();
+        System.out.println("Would you like to add an item to the list? ");
+        if(ans){
+            System.out.println("Select Fruit : Grain : Vegetable : Meat ");
+            String foodType = input.getString();
+            System.out.println("Pick quantity");
+            int quantity = input.getInt();
+            list.addToHash(foodType,quantity);
+        }else{
+            System.out.println("Have a good day");
+        }
+
     }
 
     public Set findKeys(){
